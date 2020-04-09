57080242b506a2e6fa0172bb64ee8a10f945a993
suhailmohamed <suhailmohamed333@gmail.com>
2020-04-09 22:42:34 +0530

    Updates

---
+++ precache-manifest.6273429eeaf4fb8a09125d3dec35ce1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0204921b486805f5a3b4",
+    "url": "/vajro-dashboard/static/css/main.996aa949.chunk.css"
+  },
+  {
+    "revision": "0204921b486805f5a3b4",
+    "url": "/vajro-dashboard/static/js/main.0204921b.chunk.js"
+  },
+  {
+    "revision": "d2319f6fdf86af43788c",
+    "url": "/vajro-dashboard/static/js/runtime~main.d2319f6f.js"
+  },
+  {
+    "revision": "aff8fc269612e639d32c",
+    "url": "/vajro-dashboard/static/js/2.aff8fc26.chunk.js"
+  },
+  {
+    "revision": "808ce8b7a713e7bf38434de89224f842",
+    "url": "/vajro-dashboard/index.html"
+  }
+];
